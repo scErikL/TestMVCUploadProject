a71d5e7d13e9d5a80d30dc4c0288e7e990e2223b
scErikL <erik.lovbom@yahoo.se>
2017-07-28 10:13:50 +0200

    första commit

---
+++ TestMVCUploadProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestMVCUploadProject.Startup))]
+namespace TestMVCUploadProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
